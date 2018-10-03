63d86e164a486369989a69c69febd1c6adad1be6
cd <coraldarling@outlook.com>
2018-10-03 22:28:10 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -59,7 +59,7 @@ def get_average_age_for_season(data, season)
   data.each do |season_num, lady_array|
     if season_num == season
       lady_array.each do |contestant|
-        #contestant.each do |data, value|
+        contestant.each do |data, value|
         
         
           age_array = lady_array.collect do |age| contestant["age"].to_f
