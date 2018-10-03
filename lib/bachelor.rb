dde2894472e10b313b94d5fa0e4d39277494b918
cd <coraldarling@outlook.com>
2018-10-03 23:38:06 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -68,7 +68,7 @@ def get_average_age_for_season(data, season)
           
             #average = age_array.each do |age|
              average_age = (age/count).round 
-    end
+    
   end
   average_age
 end
