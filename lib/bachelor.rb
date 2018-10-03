86b83c23c7742e3f56675f3113510b8c74478383
cd <coraldarling@outlook.com>
2018-10-03 23:54:50 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -38,7 +38,7 @@ def get_occupation(data, hometown)
   place_array = []
   data.each do |season, lady_array| 
     lady_array.each do |contestant|
-      if contestant["hometown"] = hometown
+      break if contestant["hometown"] = hometown
       #place_array = contestant.collect do |place| place = contestant["hometown"]
       #binding.pry
         #place_array.each.find do |location| 
