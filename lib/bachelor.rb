18c08fcb94658ec637c654000832550493971ce4
cd <coraldarling@outlook.com>
2018-10-03 23:27:06 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -59,7 +59,7 @@ def get_average_age_for_season(data, season)
   data[season].each do |contestant|
     count += 1.0
     age = contestant["age"].to_f
-      age += age
+    age += age
       #binding.pry  
           #age_array = lady_array.collect do #|contestant_age| contestant["age"].to_f
         
