a8dc9c75ccd133a4cc83316ac5fbb1c8e1e74a9d
cd <coraldarling@outlook.com>
2018-10-04 00:27:40 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,60 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  season_winner = []
+  data[season].each do |contestant|
+    if contestant.dig("status") == "Winner"
+      season_winner = (contestant.dig("name")).split
+    end
+  end
+  season_winner[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  name = ""
+  data.each do |season, lady_array| 
+    lady_array.each do |contestant|
+      name = contestant.fetch("name") if contestant["occupation"] == occupation
+    end
+  end
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  location_array = []
+  data.each do |season, lady_array| 
+    lady_array.each do |contestant|
+      if contestant["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  occupation = ""
+  job_array = []
+  data.each do |season, lady_array| 
+    lady_array.each do |contestant|
+      if contestant["hometown"] == hometown
+        job_array << contestant["occupation"]
+        occupation = job_array.first
+      end     
+    end
+  end
+  occupation
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  average_age = 0
+  count = 0
+  contestant_age = 0
+    data[season].each do |contestant|
+    count += 1.0
+      contestant_age += contestant["age"].to_f
+        average_age = (contestant_age.to_f/count).round
+  end
+  average_age
 end
